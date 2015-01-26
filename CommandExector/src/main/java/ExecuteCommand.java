36954c54ed03ff35129b50d4936255eabcaac107
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-01-26 21:24:25 +0200

    Add RabbitMQ receiver to the command executor.

--- CommandExector/src/main/java/ExecuteCommand.java
+++ CommandExector/src/main/java/ExecuteCommand.java
@@ -3,7 +3,11 @@
  */
 public class ExecuteCommand {
     public static void main(String[] args) {
+        /*
         Command cmd = new Command("ls -l");
         cmd.execute();
+        */
+        Receiver rec = new Receiver();
+        rec.startReceiving();
     }
 }
---
+++ CommandExector/src/main/java/Receiver.java
@@ -0,0 +1,64 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.io.IOException;
+
+/**
+ * Created by constantin on 26.01.2015.
+ */
+public class Receiver {
+    private Channel channel;
+    QueueingConsumer consumer;
+    private final static String HOST_NAME = "localhost";
+    private final static String QUEUE_NAME = "task_queue";
+
+    public Receiver() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST_NAME);
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            /*
+            * a client can be assigned only 1 message, the next message will be
+            * assigned only after it finishes processing the first message
+            */
+            int prefetchCount = 1;
+
+            channel.basicQos(prefetchCount);
+            boolean durable = true;
+            channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+            consumer = new QueueingConsumer(channel);
+            boolean autoAck = false;
+            channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startReceiving() {
+        while (true) {
+            try {
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                String message = new String(delivery.getBody());
+                doWork(message);
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                System.out.println("[X] Done");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void doWork(String command) throws InterruptedException {
+        Command cmd = new Command(command);
+        cmd.execute();
+    }
+}
