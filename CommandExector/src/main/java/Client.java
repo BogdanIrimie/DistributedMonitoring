bac0e33ac769e09ecc3fb5b870477e5fcf794e1b
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-05 17:14:05 +0200

    Rename class

--- CommandExector/src/main/java/Client.java
+++ CommandExector/src/main/java/Client.java
@@ -1,6 +1,12 @@
-public class ExecuteCommand {
+import mongo.MongoManager;
+
+public class Client {
     public static void main(String[] args) {
+        /*
         Receiver rec = new Receiver();
         rec.startReceiving();
+        */
+        MongoManager mm = new MongoManager();
+
     }
 }
