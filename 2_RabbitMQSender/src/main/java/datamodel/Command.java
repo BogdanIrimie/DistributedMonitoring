69c2eb4d4c1f10b331997597edb9eb948ef50a8b
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-06 12:37:08 +0200

    Add new datamodel.

---
+++ 2_RabbitMQSender/src/main/java/datamodel/Command.java
@@ -0,0 +1,24 @@
+package datamodel;
+
+/**
+ * Commands that can be executed by the CommandExecutor
+ */
+public class Command {
+    /**
+     * Command that can be executed
+     */
+    private String command;
+
+    public Command(String command) {
+        this.command = command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+}
