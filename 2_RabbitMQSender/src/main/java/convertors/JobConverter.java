82e0d490a3c259c18c02e5f387dbb7b0ede53a3f
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-06 12:35:50 +0200

    Change packacge.

--- 2_RabbitMQSender/src/main/java/convertors/JobConverter.java
+++ 2_RabbitMQSender/src/main/java/convertors/JobConverter.java
@@ -1,5 +1,8 @@
+package convertors;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import datamodel.Job;
 
 import java.io.IOException;
 
