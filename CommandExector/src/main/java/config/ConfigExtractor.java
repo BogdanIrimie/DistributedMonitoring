06f251079cf2d5a5ab76372ddcda4ec780c41434
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-07 23:01:07 +0200

    Add comments, add method to get all property names.

--- CommandExector/src/main/java/config/ConfigExtractor.java
+++ CommandExector/src/main/java/config/ConfigExtractor.java
@@ -2,8 +2,12 @@ package config;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Properties;
 
+/**
+ * Extract configuration/properties  from conf.properties
+ */
 public class ConfigExtractor {
     private final String configFile = "../conf.properties";
     private static Properties configProp;
@@ -21,11 +25,30 @@ public class ConfigExtractor {
 
     }
 
+    /**
+     * Read a property value from the config file
+     *
+     * @param property we want to find value for
+     * @return value of property
+     */
     public static String getProperty(String property) {
         if (configInstance == null) {
             configInstance = new ConfigExtractor();
         }
 
         return configProp.getProperty(property);
+    }
+
+
+    /**
+     * Retrieve a enumeration with all properties.
+     * @return properties
+     */
+    public static Enumeration<String> getPropertyList() {
+        if (configInstance == null) {
+            configInstance = new ConfigExtractor();
+        }
+
+        return (Enumeration<String>)configProp.propertyNames();
     }
 }
