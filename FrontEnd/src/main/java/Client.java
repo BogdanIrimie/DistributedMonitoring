097742cc71a11f1f6d06ade79472656949f7f145
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-10 15:36:54 +0200

    Change name from 2_RabbitMQSender to FontEnd

--- FrontEnd/src/main/java/Client.java
+++ FrontEnd/src/main/java/Client.java
@@ -3,7 +3,9 @@ import rabbit.Sender;
 public class Client {
     public static void main(String[] args) {
         Sender sender = new Sender();
-        sender.Send("13", "nmap -T4 -A -v info.uvt.ro");
+        for (int i = 0; i < 3; i++) {
+            sender.Send(i + "", "nmap -T4 -A -v info.uvt.ro");
+        }
         sender.closeConnection();
     }
 }
