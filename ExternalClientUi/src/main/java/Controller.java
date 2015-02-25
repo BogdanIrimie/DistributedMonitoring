8c573375e3d57e774265da671d826f8a7d159797
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-25 14:08:47 +0200

    Correct multiple instantiation of server problem.

--- ExternalClientUi/src/main/java/Controller.java
+++ ExternalClientUi/src/main/java/Controller.java
@@ -6,12 +6,12 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller {
+    private WebServer ws = new WebServer();
     public TextField command;
     @FXML
     public TextArea results;
 
     public void runCommand(ActionEvent actionEvent) {
-        WebServer ws = new WebServer();
         ws.setResults(results);
         String commandString = command.getText();
         RequestSender.sendRequest("http://localhost:8000/job", "13", commandString, "http://localhost:8008/jobFinished");
