2a4def92c24082b369ed7f87ae515870d40d4ee7
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-02-25 15:58:59 +0200

    Add comments.

--- FrontEnd/src/main/java/converters/JsonConverter.java
+++ FrontEnd/src/main/java/converters/JsonConverter.java
@@ -5,9 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+/**
+ * Convert objects from and to json.
+ */
 public class JsonConverter {
     private static ObjectMapper mapper = new ObjectMapper();
 
+    /**
+     * Convert object to json string.
+     *
+     * @param obj object that will be serialized to string.
+     * @return string with serialized object.
+     */
     public static String objectToJsonString(Object obj) {
         String objToJson = null;
         try {
@@ -18,6 +27,14 @@ public class JsonConverter {
         return objToJson;
     }
 
+    /**
+     * Convert json string into object of defined type.
+     *
+     * @param jsonString string that will be deserialize into object of type T.
+     * @param objClass class used to deserialize object.
+     * @param <T> type to be used to deserialize.
+     * @return object of type T.
+     */
     public static <T> T jsonStringToObject(String jsonString, Class<T> objClass) {
         T object = null;
         try {
@@ -29,4 +46,5 @@ public class JsonConverter {
 
         return object;
     }
+
 }
