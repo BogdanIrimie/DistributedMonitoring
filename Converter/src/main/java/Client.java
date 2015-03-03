2a20c03300678bcd7bbef0b6470fcec3396b2d6c
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-03-03 15:26:38 +0200

    Add info messages.

--- Converter/src/main/java/Client.java
+++ Converter/src/main/java/Client.java
@@ -1,7 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rabbit.Receiver;
 
 public class Client {
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
     public static void main(String[] args) {
+        logger.info("Converter is started!");
         Receiver rec = new Receiver();
         rec.startReceiving();
     }
--- Scanner/src/main/java/Client.java
+++ Scanner/src/main/java/Client.java
@@ -1,10 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rabbit.Receiver;
 
 public class Client {
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(Client.class);
 
+    public static void main(String[] args) {
+        logger.info("Scaner is started!");
         Receiver rec = new Receiver();
         rec.startReceiving();
-
     }
 }
