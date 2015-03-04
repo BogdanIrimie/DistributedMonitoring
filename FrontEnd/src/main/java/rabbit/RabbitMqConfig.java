43828b4bc8c80b0bd4d4ee2cb5a10c50d9d4beb2
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-03-04 15:50:47 +0200

    Add user and password in config file for RabbitMQ.

--- FrontEnd/src/main/java/rabbit/RabbitMqConfig.java
+++ FrontEnd/src/main/java/rabbit/RabbitMqConfig.java
@@ -8,10 +8,14 @@ import config.ConfigExtractor;
 public class RabbitMqConfig {
     private String host = null;
     private String queue = null;
+    private String username = null;
+    private String password = null;
 
     public RabbitMqConfig() {
         host = ConfigExtractor.getProperty("rabbitHost");
         queue = ConfigExtractor.getProperty("rabbitSendQueue");
+        username = ConfigExtractor.getProperty("rabbitUser");
+        password = ConfigExtractor.getProperty("rabbitPassword");
     }
 
     public String getHost() {
@@ -22,4 +26,19 @@ public class RabbitMqConfig {
         return queue;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
