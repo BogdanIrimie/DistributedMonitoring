60caa3057f8cf419dc941ae019c131cc1f0aa284
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-03-29 14:19:01 +0300

    Add jsonConverter.

---
+++ ExternalClientUi/src/main/java/converters/JsonConverter.java
@@ -0,0 +1,50 @@
+package converters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * Convert objects from and to json.
+ */
+public class JsonConverter {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Convert object to json string.
+     *
+     * @param obj object that will be serialized to string.
+     * @return string with serialized object.
+     */
+    public static String objectToJsonString(Object obj) {
+        String objToJson = null;
+        try {
+            objToJson = mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.getStackTrace();
+        }
+        return objToJson;
+    }
+
+    /**
+     * Convert json string into object of defined type.
+     *
+     * @param jsonString string that will be deserialize into object of type T.
+     * @param objClass class used to deserialize object.
+     * @param <T> type to be used to deserialize.
+     * @return object of type T.
+     */
+    public static <T> T jsonStringToObject(String jsonString, Class<T> objClass) {
+        T object = null;
+        try {
+            object = mapper.readValue(jsonString, objClass);
+        }
+        catch (IOException e) {
+            e.getStackTrace();
+        }
+
+        return object;
+    }
+
+}
