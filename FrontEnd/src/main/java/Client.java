941ccc505df6f40c3506caad92d4cc5272f53037
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-04-30 11:36:47 +0300

    Use process id in log file names to distinguish between different instances of the same component.

--- FrontEnd/src/main/java/Client.java
+++ FrontEnd/src/main/java/Client.java
@@ -1,8 +1,16 @@
+import org.slf4j.MDC;
 import webserver.WebServer;
 
 public class Client {
 
     public static void main(String[] args) {
+        MDC.put("pid", getPid());
         WebServer ws = new WebServer();
+        MDC.clear();
+    }
+
+    private static String getPid() {
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        return processName.split("@")[0];
     }
 }
