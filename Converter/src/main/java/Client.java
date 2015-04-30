593a6b30b44e6d53d4025416117939cc5294648e
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-04-30 14:44:16 +0300

    Set pid in MDR

--- Converter/src/main/java/Client.java
+++ Converter/src/main/java/Client.java
@@ -1,13 +1,20 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import rabbit.Receiver;
 
 public class Client {
     private static final Logger logger = LoggerFactory.getLogger(Client.class);
     public static void main(String[] args) {
+        MDC.put("pid", getPid());
         logger.info("Converter is started!");
         Receiver rec = new Receiver();
         rec.startReceiving();
+        MDC.clear();
     }
 
+    private static String getPid() {
+        String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+        return processName.split("@")[0];
+    }
 }
