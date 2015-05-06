dd996961b8a12f9c79948b678db2d79e609507d5
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-05-06 12:13:55 +0300

    Use the new ciphersuite csv

--- Converter/src/main/java/processors/TlsEcrypt2Level.java
+++ Converter/src/main/java/processors/TlsEcrypt2Level.java
@@ -1,6 +1,7 @@
 package processors;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.opencsv.CSVReader;
 import converters.JsonConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,17 +12,29 @@ import java.util.*;
 public class TlsEcrypt2Level implements Processor {
     public static final Logger logger = LoggerFactory.getLogger(TlsEcrypt2Level.class);
     private static Map<String, Integer> ciphersLevel = new LinkedHashMap<String, Integer>();
+    private List<Ciphersuite> ciphersuites;
 
     public TlsEcrypt2Level() {
-        // read from file the ciphersuites
+        ClassLoader classLoader = getClass().getClassLoader();
+        BufferedReader br = null;
+        ciphersuites = new ArrayList<Ciphersuite>();
         try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            BufferedReader br = new BufferedReader(new FileReader(classLoader.getResource("ciphersuites.csv").getFile()));
-            String line;
-            br.readLine();  // the first row with column head
-            while ((line = br.readLine()) != null) {
-                String[] columns = line.split(",");
-                ciphersLevel.put(columns[0].trim(), Integer.parseInt(columns[1].trim())); // columns[0] cipher; columns[1] encrypt2level
+            CSVReader reader = new CSVReader(new FileReader(classLoader.getResource("ciphersuites2.csv").getFile()));
+            reader.readNext();
+            String[] attributes;
+            while ((attributes = reader.readNext()) != null) {
+                Ciphersuite ciphersuite = new Ciphersuite();
+
+                ciphersuite.setCipherId(Integer.parseInt(attributes[0].trim().substring(2), 16));
+                ciphersuite.setName(attributes[1]);
+                ciphersuite.setProtocol(attributes[2]);
+                ciphersuite.setKeyExchange(attributes[3]);
+                ciphersuite.setAuthentication(attributes[4]);
+                ciphersuite.setSymetricEncryptionAlgorithm(attributes[5]);
+                ciphersuite.setBits(Integer.parseInt(attributes[6].trim().isEmpty() ? "0" : attributes[6].trim()));
+                ciphersuite.setHashAlgorithm(attributes[7]);
+
+                ciphersuites.add(ciphersuite);
             }
         } catch (FileNotFoundException e) {
             logger.error(e.getMessage(), e);
@@ -71,6 +84,82 @@ public class TlsEcrypt2Level implements Processor {
 
         public void setEcript2Level(int ecript2Level) {
             this.ecript2Level = ecript2Level;
+        }
+    }
+
+
+    private class Ciphersuite {
+        private int cipherId;
+        private String name;
+        private String protocol;
+        private String keyExchange;
+        private String authentication;
+        private String symetricEncryptionAlgorithm;
+        private int bits;
+        private String hashAlgorithm;
+
+        public int getCipherId() {
+            return cipherId;
+        }
+
+        public void setCipherId(int cipherId) {
+            this.cipherId = cipherId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getProtocol() {
+            return protocol;
+        }
+
+        public void setProtocol(String protocol) {
+            this.protocol = protocol;
+        }
+
+        public String getKeyExchange() {
+            return keyExchange;
+        }
+
+        public void setKeyExchange(String keyExchange) {
+            this.keyExchange = keyExchange;
+        }
+
+        public String getAuthentication() {
+            return authentication;
+        }
+
+        public void setAuthentication(String authentication) {
+            this.authentication = authentication;
+        }
+
+        public String getSymetricEncryptionAlgorithm() {
+            return symetricEncryptionAlgorithm;
+        }
+
+        public void setSymetricEncryptionAlgorithm(String symetricEncryptionAlgorithm) {
+            this.symetricEncryptionAlgorithm = symetricEncryptionAlgorithm;
+        }
+
+        public int getBits() {
+            return bits;
+        }
+
+        public void setBits(int bits) {
+            this.bits = bits;
+        }
+
+        public String getHashAlgorithm() {
+            return hashAlgorithm;
+        }
+
+        public void setHashAlgorithm(String hashAlgorithm) {
+            this.hashAlgorithm = hashAlgorithm;
         }
     }
 
