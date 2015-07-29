21762e600018a6d8c3ca4a125bec8216726809cf
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-07-29 19:08:44 +0300

    Add Remediator component.

---
+++ Remediator/src/main/java/datamodel/Measurement.java
@@ -0,0 +1,107 @@
+package datamodel;
+
+/**
+ * Model for data in DB
+ */
+public class Measurement {
+    private MongoId _id;
+    private String clientId;
+    private String clientIp;
+    private String command;
+    private String responseAddress;
+    private String[] processors;
+    private String adapter;
+    private String rawResult;
+    private String processedResult;
+
+    public Measurement() {}
+
+    public Measurement(String clientId, String command, String responseAddress) {
+        this.clientId = clientId;
+        this.command = command;
+        this.responseAddress = responseAddress;
+    }
+
+    public Measurement(String clientId, String clientIp, String command, String responseAddress, String[] processors, String adapter) {
+        this.clientId = clientId;
+        this.clientIp = clientIp;
+        this.command = command;
+        this.responseAddress = responseAddress;
+        this.processors = processors;
+        this.adapter = adapter;
+    }
+
+
+    public MongoId get_id() {
+        return _id;
+    }
+
+    public void set_id(MongoId _id) {
+        this._id = _id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public void setClientIp(String clientIp) {
+        this.clientIp = clientIp;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getResponseAddress() {
+        return responseAddress;
+    }
+
+    public void setResponseAddress(String responseAddress) {
+        this.responseAddress = responseAddress;
+    }
+
+    public String[] getProcessors() {
+        return processors;
+    }
+
+    public void setProcessors(String[] processors) {
+        this.processors = processors;
+    }
+
+    public String getAdapter() {
+        return adapter;
+    }
+
+    public void setAdapter(String adapter) {
+        this.adapter = adapter;
+    }
+
+    public String getRawResult() {
+        return rawResult;
+    }
+
+    public void setRawResult(String rawResult) {
+        this.rawResult = rawResult;
+    }
+
+    public String getProcessedResult() {
+        return processedResult;
+    }
+
+    public void setProcessedResult(String processedResult) {
+        this.processedResult = processedResult;
+    }
+
+}
---
+++ Remediator/src/main/java/mongo/MongoDbConfig.java
@@ -0,0 +1,44 @@
+package mongo;
+
+import config.ConfigExtractor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Extract configuration data for MongoDB
+ */
+public class MongoDbConfig {
+    private static final Logger logger = LoggerFactory.getLogger(MongoDbConfig.class);
+    private String ip;
+    private int port;
+
+    /**
+     * read configuration data for mongoDB
+     */
+    public MongoDbConfig() {
+        ip = ConfigExtractor.getProperty("mongoHost");
+        try {
+            port = Integer.parseInt(ConfigExtractor.getProperty("mongoPort"));
+        }
+        catch (NumberFormatException nfe) {
+            logger.error(nfe.getMessage(), nfe);
+        }
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+}
