0444e21dfb33dcb684eba2d5fcff5198209baf94
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-08-13 15:12:16 +0300

    Remove unused imports

--- Converter/src/main/java/processors/ProcessorsExecutor.java
+++ Converter/src/main/java/processors/ProcessorsExecutor.java
@@ -1,12 +1,10 @@
 package processors;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import datamodel.Job;
 import datamodel.Measurement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ProcessorsExecutor {
