a8be0288ec04b14c006de20115670aff4bef0586
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-08-22 21:49:17 +0300

    Aloow request to be easily customized.

--- ExternalClient/src/main/java/requests/CiphersuiteRequest.java
+++ ExternalClient/src/main/java/requests/CiphersuiteRequest.java
@@ -3,17 +3,31 @@ package requests;
 import datamodel.Request;
 import httpserver.RequestSender;
 
+/*
+*   Request ciphersuite list
+* */
 public class CiphersuiteRequest implements Runnable {
 
+    private String responseAddress;
+    private String sendRequestAddress;
+    private String scanTargetAddress;
+
+    public CiphersuiteRequest(String responseAddress, String sendRequestAddress, String scanTargetAddress) {
+        this.responseAddress = responseAddress;
+        this.sendRequestAddress = sendRequestAddress;
+        this.scanTargetAddress = scanTargetAddress;
+    }
+
     public void run() {
-        String commandString = "nmap --script ssl-enum-ciphers -p 443 web.info.uvt.ro";
+        String commandString = "nmap --host-timeout 8s --script ssl-enum-ciphers -p 443 " + scanTargetAddress;
 
         Request request = new Request();
         request.setClientId("13");
         request.setCommand(commandString);
-        request.setResponseAddress("http://localhost:8008/jobFinished");
+        request.setResponseAddress("http://" + responseAddress + ":8008/jobFinished");
         request.setProcessors(new String[]{"processors.XmlToJsonConverter", "processors.TlsCiphersuitesFilter"});
         request.setAdapter("adapters.EventHubAdapter");
-        String requestResponse = RequestSender.sendRequest("http://localhost:8000/job", request);
+        RequestSender rs = new RequestSender();
+        String requestResponse = rs.sendRequest("http://" + sendRequestAddress + ":8000/job", request);
     }
 }
--- ExternalClient/src/main/java/requests/OpenPortRequest.java
+++ ExternalClient/src/main/java/requests/OpenPortRequest.java
@@ -5,16 +5,27 @@ import httpserver.RequestSender;
 
 public class OpenPortRequest implements Runnable{
 
+    private String responseAddress;
+    private String sendRequestAddress;
+    private String scanTargetAddress;
+
+    public OpenPortRequest(String responseAddress, String sendRequestAddress, String scanTargetAddress) {
+        this.responseAddress = responseAddress;
+        this.sendRequestAddress = sendRequestAddress;
+        this.scanTargetAddress = scanTargetAddress;
+    }
+
     public void run() {
-        String commandString = "nmap --open web.info.uvt.ro";
+        String commandString = "nmap --host-timeout 8s --open " + scanTargetAddress;
 
         Request request = new Request();
         request.setClientId("13");
         request.setCommand(commandString);
-        request.setResponseAddress("http://localhost:8008/jobFinished");
+        request.setResponseAddress("http://" + responseAddress + ":8008/jobFinished");
         request.setProcessors(new String[]{"processors.XmlToJsonConverter"});
         request.setAdapter("adapters.EventHubAdapter");
-        String requestResponse = RequestSender.sendRequest("http://localhost:8000/job", request);
+        RequestSender rs = new RequestSender();
+        String requestResponse = rs.sendRequest("http://" + sendRequestAddress + ":8000/job", request);
 
     }
 }
