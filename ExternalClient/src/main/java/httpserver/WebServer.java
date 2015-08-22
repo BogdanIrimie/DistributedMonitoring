9982263582bcfd635ff4887d1076409f2d040d5c
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-08-22 21:49:50 +0300

    New logic for server so it can count the number of connection and start new test runs.

--- ExternalClient/src/main/java/httpserver/WebServer.java
+++ ExternalClient/src/main/java/httpserver/WebServer.java
@@ -4,23 +4,37 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import javafx.scene.control.TextArea;
+import requests.RequestExecutor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebServer {
 
+    private static final int TOTAL_SCANS = 1000;
+    private static int TOTAL_RUNS = 5;
+    private static int run = 1;
     private static TextArea results = null;
+    private static int resultsCounter = 0;
+    private static long startTime, endTime;
+    private List<Long> runTimes = new ArrayList<Long>();
+    private HttpServer server = null;;
 
     public WebServer() {
         this(8008);
     }
 
+    public WebServer(int port, int totalRuns) {
+        this(port);
+        TOTAL_RUNS = totalRuns;
+    }
+
     public WebServer(int port) {
-        HttpServer server = null;
         try {
             server = HttpServer.create(new InetSocketAddress(port), 0);
         } catch (IOException e) {
@@ -30,6 +44,7 @@ public class WebServer {
         server.setExecutor(null); // creates a default executor
         server.start();
         System.out.println("Server started listening on port 8008.");
+        startTime = System.currentTimeMillis();
     }
 
     public static TextArea getResults() {
@@ -51,14 +66,41 @@ public class WebServer {
                 receivedData.append(line);
             }
 
-            System.out.println(receivedData.toString());
+            //System.out.println(receivedData.toString());
 
-
-            System.out.println("Received data: " + receivedData.toString());
+            //System.out.println("Received data: " + receivedData.toString());
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
+            if (resultsCounter == 0) {
+                System.out.print("Run " + run + ": ");
+            }
+            resultsCounter++;
+            System.out.print(resultsCounter + " ");
+            if (resultsCounter == TOTAL_SCANS) {
+                endTime = System.currentTimeMillis();
+                long elapsedTime = (endTime - startTime)/1000;
+                System.out.println(" -- time elapsed: " +  elapsedTime + " seconds");
+                runTimes.add(elapsedTime);
+                run++;
+                resultsCounter = 0;
+                if (run <= TOTAL_RUNS) {
+                    startTime = System.currentTimeMillis();
+                    RequestExecutor.generateRequests();
+                }
+                else {
+                    int i = 0;
+                    System.out.println("run, time");
+                    for (Long time : runTimes) {
+                        i++;
+                        System.out.println(i + ", " + time);
+                    }
+                    if (server != null) {
+                        server.stop(1);
+                    }
+                }
+            }
         }
     }
 
