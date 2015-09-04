c7f046decc46f7c134033a2cd29419d47a44ee00
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-09-04 17:12:26 +0100

    Update IP addresses and test count.

--- ExternalClient/src/main/java/requests/RequestExecutor.java
+++ ExternalClient/src/main/java/requests/RequestExecutor.java
@@ -6,19 +6,19 @@ public class RequestExecutor {
     public static void generateRequests() {
 
         final String responseAddress = "104.155.8.70";
-        final String sendRequestAddress = "146.148.126.140";
+        final String sendRequestAddress = "104.155.16.251";
         final String scanTargetAddress = "104.155.44.242";
         final String scanTargetAddress2 = "";
 
-        for (int i = 0; i < 334; i++ ) {
+        for (int i = 0; i < 4; i++ ) {
             new Thread(new CiphersuiteRequest(responseAddress, sendRequestAddress, scanTargetAddress)).start();
         }
 
-        for (int i = 0; i < 333; i++) {
+        for (int i = 0; i < 3; i++) {
             new Thread(new Ecrypt2LevelRequest(responseAddress, sendRequestAddress, scanTargetAddress)).start();
         }
 
-        for(int i = 0; i < 333; i++ ) {
+        for(int i = 0; i < 3; i++ ) {
             new Thread(new OpenPortRequest(responseAddress, sendRequestAddress, scanTargetAddress)).start();
         }
     }
