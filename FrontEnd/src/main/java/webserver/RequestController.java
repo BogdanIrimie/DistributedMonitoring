73312d4d213466f744b0cb371370c103d76e69ed
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-09-04 15:34:49 +0100

    Open connection to database and message queue in PostConstruct and close it PreDestroy. Before this, application was creating a DB and message queue connection for every request.

--- FrontEnd/src/main/java/webserver/RequestController.java
+++ FrontEnd/src/main/java/webserver/RequestController.java
@@ -11,23 +11,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import rabbit.Sender;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class RequestController {
+    private Sender sender;
+
+    @PostConstruct
+    private void instantiateSender() {
+        sender = new Sender();
+    }
 
     @RequestMapping(value = "/request", method = RequestMethod.POST)
     public ResponseEntity<Job> jobRequest(@RequestBody String requestBody, HttpServletRequest httpRequest) {
         Request request = JsonConverter.jsonStringToObject(requestBody, Request.class);
 
         if ((request.getClientId() != null) && (request.getCommand() != null)) {
-            Sender sender = new Sender();
             String jobId = sender.send(request, httpRequest.getRemoteAddr());
-            sender.closeConnection();
             return new ResponseEntity<Job>(new Job(jobId), HttpStatus.ACCEPTED);
         }
         else {
             return new ResponseEntity<Job>(new Job("-1"), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @PreDestroy
+    private void closeSender() {
+        sender.closeConnection();
     }
 }
