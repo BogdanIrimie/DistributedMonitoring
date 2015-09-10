509dde68dee7083dde4a07268f54c352aadc4777
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-09-10 14:14:14 +0100

    Add datamodel for self performance results.

---
+++ Scanner/src/main/java/datamodel/PerformanceSelfMonitoring.java
@@ -0,0 +1,13 @@
+package datamodel;
+
+public class PerformanceSelfMonitoring {
+    private CpuUsageResults cpuUsageResults;
+
+    public CpuUsageResults getCpuUsageResults() {
+        return cpuUsageResults;
+    }
+
+    public void setCpuUsageResults(CpuUsageResults cpuUsageResults) {
+        this.cpuUsageResults = cpuUsageResults;
+    }
+}
