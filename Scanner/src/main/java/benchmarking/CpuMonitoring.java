22011a6af393fb048a5ab764899eca4b7eda9509
Bogdan Irimie <irimie.bogdan@outlook.com>
2015-09-15 14:50:46 +0100

    Read lines from the end of the file, to reduce the amout of data read from the file and CPu usage.

--- Scanner/src/main/java/benchmarking/CpuMonitoring.java
+++ Scanner/src/main/java/benchmarking/CpuMonitoring.java
@@ -1,15 +1,16 @@
 package benchmarking;
 
 import datamodel.CpuUsageResults;
+import org.apache.commons.io.input.ReversedLinesFileReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 
 public class CpuMonitoring implements MonitoringInterface {
@@ -35,61 +36,66 @@ public class CpuMonitoring implements MonitoringInterface {
         String line;
         List<String> cpuUsageResults = new ArrayList<String>();
 
-/*
+
         try {
+            Thread.sleep(1500);
             String strpath="/tmp/everySecondMonitoring.txt";
             ReversedLinesFileReader fr = new ReversedLinesFileReader(new File(strpath));;
             do {
                 line = fr.readLine();
-                String[] splitedLine = line.split("\\s+");
-
-                if ((splitedLine.length > 3) &&                                                                         // at least a timestamp, a pid and a %
-                        splitedLine[1].trim().length() > 0 &&                                                           // the PID length is > 0
-                        isNumeric(splitedLine[1].trim()) &&                                                             // the PID is a number
-                        (splitedLine[0].trim().length() > 18)) {                                                        // a valid timestamp (date + time)
-
-                    long resultsPid = Long.parseLong(splitedLine[1]);
-                    if (resultsPid == pid) {
-                        String[] dateAndTime = splitedLine[0].split("_");
-                        String date = dateAndTime[0];
-                        String time = dateAndTime[1];
-
-                        if (date.trim().length() == 0 || time.trim().length() == 0) {
-                            continue;
-                        }
-
-                        String[] dateSplit = date.split("/");
-                        String[] timeSplit = time.split(":");
-
-                        Calendar timeFromLog = Calendar.getInstance();
-
-                        timeFromLog.set(Calendar.SECOND, Integer.parseInt(timeSplit[2]));
-                        timeFromLog.set(Calendar.MINUTE, Integer.parseInt(timeSplit[1]));
-                        timeFromLog.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeSplit[0]));
-                        timeFromLog.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateSplit[2]));
-                        timeFromLog.set(Calendar.MONTH, Integer.parseInt(dateSplit[1]) - 1);
-                        timeFromLog.set(Calendar.YEAR, Integer.parseInt(dateSplit[0]));
-
-                        if (timeFromLog.after(startTimeOfMonitoring) && timeFromLog.before(endTimeOfMonitoring)) {
-                            cpuUsageResults.add(splitedLine[2]);                                                        // PID is ok and time is ok
-                        }
-
-                        // look untill the time read from log is before the process began
-                        if (timeFromLog.before(startTimeOfMonitoring)) {
-                            break;
-                        }
-                    }
+                if (line == null) {
+                    continue;
                 }
 
+                String[] splitedLine = line.split("\\s+");
+
+                if ((splitedLine.length > 2) &&                                                                         // at least a timestamp, a pid and a %
+                        splitedLine[1].trim().length() > 0 &&                                                           // the PID length is > 0
+                        (splitedLine[0].trim().length() > 18)) {                                                        // a valid timestamp (date + time)
+
+                    String[] dateAndTime = splitedLine[0].split("_");
+                    String date = dateAndTime[0];
+                    String time = dateAndTime[1];
+
+                    if (date.trim().length() == 0 || time.trim().length() == 0) {
+                        continue;
+                    }
+
+                    String[] dateSplit = date.split("/");
+                    String[] timeSplit = time.split(":");
+
+                    Calendar timeFromLog = Calendar.getInstance();
+
+                    timeFromLog.set(Calendar.SECOND, Integer.parseInt(timeSplit[2]));
+                    timeFromLog.set(Calendar.MINUTE, Integer.parseInt(timeSplit[1]));
+                    timeFromLog.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeSplit[0]));
+                    timeFromLog.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateSplit[2]));
+                    timeFromLog.set(Calendar.MONTH, Integer.parseInt(dateSplit[1]) - 1);
+                    timeFromLog.set(Calendar.YEAR, Integer.parseInt(dateSplit[0]));
+
+                    if (timeFromLog.after(startTimeOfMonitoring) && timeFromLog.before(endTimeOfMonitoring) && isNumeric(splitedLine[1].trim())) {
+                        long resultsPid = Long.parseLong(splitedLine[1]);
+                        if (resultsPid == pid) {
+                            cpuUsageResults.add(splitedLine[2]);                                                    // PID is ok and time is ok
+                        }
+                    }
+
+                    // look untill the time read from log is before the process began
+                    if (timeFromLog.before(startTimeOfMonitoring)) {
+                        break;
+                    }
+                }
             } while (line != null);
             fr.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
         }
+        Collections.reverse(cpuUsageResults);
 
-*/
 
-
+/*
         try {
             // wait for 1 second before reading the file so that all data for PID is written in file
             Thread.sleep(1500);
@@ -148,7 +154,7 @@ public class CpuMonitoring implements MonitoringInterface {
         } catch (InterruptedException e) {
             logger.error(e.getMessage(), e);
         }
-
+*/
         return new CpuUsageResults(pid, startTimeOfMonitoring, cpuUsageResults);
     }
 
