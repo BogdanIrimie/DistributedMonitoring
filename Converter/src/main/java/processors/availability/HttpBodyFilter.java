e88a4564863d273ea0920283379bc2f794df340d
Irimie Bogdan <irimie.bogdan@outlook.com>
2015-11-18 13:34:53 +0200

    Enhance processor for http response body.

--- Converter/src/main/java/processors/availability/HttpBodyFilter.java
+++ Converter/src/main/java/processors/availability/HttpBodyFilter.java
@@ -1,6 +1,11 @@
 package processors.availability;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import converters.JsonConverter;
 import datamodel.HttpResponseForAvailability;
 import processors.Processor;
@@ -10,6 +15,12 @@ public class HttpBodyFilter implements Processor {
     @Override
     public String process(String textToProcess, JsonNode configuration) throws Throwable {
         HttpResponseForAvailability httpResponseForAvailability = JsonConverter.jsonStringToObject(textToProcess, HttpResponseForAvailability.class);
-        return httpResponseForAvailability.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String[] ignorableFieldNames = { "httpStatusCode"};
+        FilterProvider filters = new SimpleFilterProvider()
+                .addFilter("httpFilter", SimpleBeanPropertyFilter.serializeAllExcept(ignorableFieldNames));
+        ObjectWriter writer = mapper.writer(filters);
+        return  writer.writeValueAsString(httpResponseForAvailability);
     }
 }
