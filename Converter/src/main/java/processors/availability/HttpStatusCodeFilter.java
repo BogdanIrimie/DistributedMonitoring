bf537af32efd565fb342cbe04217392c58484445
Irimie Bogdan <irimie.bogdan@outlook.com>
2015-11-18 11:18:01 +0200

    Enchance status code filter to output a valid json containing the url and http status code.

--- Converter/src/main/java/processors/availability/HttpStatusCodeFilter.java
+++ Converter/src/main/java/processors/availability/HttpStatusCodeFilter.java
@@ -1,15 +1,32 @@
 package processors.availability;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import converters.JsonConverter;
 import datamodel.HttpResponseForAvailability;
 import processors.Processor;
+
 
 public class HttpStatusCodeFilter implements Processor {
 
     @Override
     public String process(String textToProcess, JsonNode configuration) throws Throwable {
-        HttpResponseForAvailability httpResponseForAvailability = JsonConverter.jsonStringToObject(textToProcess, HttpResponseForAvailability.class);
-        return httpResponseForAvailability.getStatusCode();
+        HttpResponseForAvailability httpResponseForAvailability =
+                JsonConverter.jsonStringToObject(textToProcess, HttpResponseForAvailability.class);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        String[] ignorableFieldNames = { "body"};
+        FilterProvider filters = new SimpleFilterProvider()
+                .addFilter("httpFilter",SimpleBeanPropertyFilter.serializeAllExcept(ignorableFieldNames));
+        ObjectWriter writer = mapper.writer(filters);
+        String filteredJson = writer.writeValueAsString(httpResponseForAvailability);
+
+        return filteredJson;
     }
+
 }
