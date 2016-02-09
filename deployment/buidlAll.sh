326f4a5eda5ee89ab87739a930a505f5ccd625f2
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-02-09 14:56:11 +0200

    Remove log files when building the components. Add comments.

--- deployment/buidlAll.sh
+++ deployment/buidlAll.sh
@@ -4,6 +4,7 @@ cd ..
 projectPath=$(pwd);
 echo $projectPath;
 
+# Maven build.
 echo ------ Start building FrontEnd component ------
 mvn -Dmaven.test.skip=true package -f $projectPath/FrontEnd;
 echo ------ Finish FrontEnd component build ------
@@ -20,11 +21,19 @@ echo ------ Start building Presenter component ------
 mvn -Dmaven.test.skip=true package -f $projectPath/Presenter;
 echo ------ Finish Presenter component build ------
 
+# Copy .jar files to deployment folder.
 cp $projectPath/FrontEnd/target/FrontEnd.jar $projectPath/deployment/specs_monitoring_nmap_frontend/lib
 cp $projectPath/Scanner/target/Scanner.jar $projectPath/deployment/specs_monitoring_nmap_scanner/lib
 cp $projectPath/Converter/target/Converter.jar $projectPath/deployment/specs_monitoring_nmap_converter/lib
 cp $projectPath/Presenter/target/Presenter.jar $projectPath/deployment/specs_monitoring_nmap_presenter/lib
 
+# Remove all logs from the deployment archives.
+rm -rf $projectPath/deployment/specs_monitoring_nmap_frontend/var/*
+rm -rf $projectPath/deployment/specs_monitoring_nmap_scanner/var/*
+rm -rf $projectPath/deployment/specs_monitoring_nmap_converter/var/*
+rm -rf $projectPath/deployment/specs_monitoring_nmap_presenter/var/*
+
+# Create deployment archives for all components.
 tar -cf $projectPath/deployment/specs_monitoring_nmap_frontend.tar.gz $projectPath/deployment/specs_monitoring_nmap_frontend
 tar -cf $projectPath/deployment/specs_monitoring_nmap_scanner.tar.gz $projectPath/deployment/specs_monitoring_nmap_scanner
 tar -cf $projectPath/deployment/specs_monitoring_nmap_converter.tar.gz $projectPath/deployment/specs_monitoring_nmap_converter
