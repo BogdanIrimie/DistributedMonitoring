ce6dd2076d29ff65c7e7575a86a0d2c8ec4a9585
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-02-12 17:27:49 +0200

    Remove code for generating config file because it is generated by chef recipe.

--- deployment/specs_monitoring_nmap_converter/bin/run.sh
+++ deployment/specs_monitoring_nmap_converter/bin/run.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
 
-env=printenv;
-
-mkdir -p ../etc
-exec > ../etc/conf.properties
-echo '#RabbitMQ connection details'
-$env | grep rabbitHost
-$env | grep coRabbitSendQueue | sed 's/coRabbitSendQueue/rabbitSendQueue/g'
-$env | grep coRabbitReceiveQueue | sed 's/coRabbitReceiveQueue/rabbitReceiveQueue/g'
-$env | grep rabbitUser
-$env | grep rabbitPassword
-echo 
-echo '#MongoDB connection details'
-$env | grep mongoHost
-$env | grep mongoPort
-
-exec > ../var/specs_monitoring_nmap_converter.pid
-java -jar ../lib/Converter.jar & echo $!
+java -jar ../lib/Converter.jar -p specs_monitoring_nmap_converter.pid
--- deployment/specs_monitoring_nmap_presenter/bin/run.sh
+++ deployment/specs_monitoring_nmap_presenter/bin/run.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
 
-env=printenv;
-
-mkdir -p ../etc
-exec > ../etc/conf.properties
-echo '#RabbitMQ connection details'
-$env | grep rabbitHost
-$env | grep prRabbitSendQueue | sed 's/prRabbitSendQueue/rabbitSendQueue/g'
-$env | grep prRabbitReceiveQueue | sed 's/prRabbitReceiveQueue/rabbitReceiveQueue/g'
-$env | grep rabbitUser
-$env | grep rabbitPassword
-echo 
-echo '#MongoDB connection details'
-$env | grep mongoHost
-$env | grep mongoPort
-
-exec > ../var/specs_monitoring_nmap_presenter.pid
-java -jar ../lib/Presenter.jar & echo $!
+java -jar ../lib/Presenter.jar -p specs_monitoring_nmap_presenter.pid
