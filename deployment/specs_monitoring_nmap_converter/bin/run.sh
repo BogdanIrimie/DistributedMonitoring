288aaeebff6617d0752d269b04a023680899a8a1
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-02-19 16:16:25 +0200

    Reload profile data and use JAVA_HOME in start script.

--- deployment/specs_monitoring_nmap_converter/bin/run.sh
+++ deployment/specs_monitoring_nmap_converter/bin/run.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-java -jar ../lib/Converter.jar -p specs_monitoring_nmap_converter.pid
+# Reload profile if it exists.
+if [ -f /etc/profileT ];
+then
+    echo "Reload profile data"
+    source /etc/profile
+fi
+
+# Use JAVA_HOME if it is set.
+if [ ! -z "$JAVA_HOME" ];
+then
+    # JAVA_HOME is set, use it.
+    $JAVA_HOME/bin/java -jar ../lib/Converter.jar -p specs_monitoring_nmap_converter.pid
+else
+    # JAVA_HOME is NOT set.
+    java -jar ../lib/Converter.jar -p specs_monitoring_nmap_converter.pid 
+fi
