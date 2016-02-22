f1c50e61caa609cbe7af03240a3917f70d1bcf83
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-02-22 16:10:27 +0200

    Add stop script for all components. Rename start script from "run.sh" to "start.sh".

---
+++ deployment/specs_monitoring_nmap_scanner/bin/stop.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Check if PID file exists.
+if [ ! -f ../var/specs_monitoring_nmap_scanner.pid ];
+then
+    echo "No PID file found!"
+    exit -1
+fi
+
+# Read PID from file.
+file="../var/specs_monitoring_nmap_scanner.pid"
+PID=$(cat "$file")
+
+# Check if file content is a number.
+numberRegex='^[0-9]+$'
+if ! [[ $PID =~ $numberRegex ]];
+then
+    echo "The content of the file is not a valid PID!"
+    exit -2
+fi
+
+# Kill running process.
+if kill $PID;
+then
+    echo "Process with PID $PID was killed!"
+else
+    echo "There is no process with $PID running!"
+fi
