516495ce7c4d80c7a67724a6e272f0015b2dbd4c
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-02-22 14:57:24 +0200

    Update startup script for all components. Use JAVA_HOME if it is set.

--- deployment/specs_monitoring_nmap_frontend/bin/run.sh
+++ deployment/specs_monitoring_nmap_frontend/bin/run.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-java -jar ../lib/FrontEnd.jar -p ../var/specs_monitoring_nmap_frontend.pid
+# Reload profile if it exists.
+if [ -f /etc/profileT ];
+then
+    echo "Reload profile data"
+    source /etc/profile
+fi
+
+# USe JAVA_HOME if it is set.
+if [ ! -z "$JAVA_HOME" ];
+then
+    # JAVA_HOME is set, use it.
+    $JAVA_HOME/bin/java -jar ../lib/FrontEnd.jar -p ../var/specs_monitoring_nmap_frontend.pid
+else
+    # JAVA_HOME is NOT set.
+    java -jar ../lib/FrontEnd.jar -p ../var/specs_monitoring_nmap_frontend.pid
+fi
--- deployment/specs_monitoring_nmap_scanner/bin/run.sh
+++ deployment/specs_monitoring_nmap_scanner/bin/run.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-java -jar ../lib/Scanner.jar -p specs_monitoring_nmap_scanner.pid
+# Reload profile if it exists.
+if [ -f /etc/profileT ];
+then
+    echo "Reload profile data"
+    source /etc/profile
+fi
+
+# Use JAVA_HOME if it is set.
+if [ ! -z "$JAVA_HOME" ];
+then
+    # JAVA_HOME is set, use it.
+    $JAVA_HOME/bin/java -jar ../lib/Scanner.jar -p specs_monitoring_nmap_scanner.pid
+else
+    # JAVA_HOME is NOT set.
+    java -jar ../lib/Scanner.jar -p specs_monitoring_nmap_scanner.pid
+fi
