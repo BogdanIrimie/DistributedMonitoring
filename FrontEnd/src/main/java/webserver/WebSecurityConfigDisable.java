76d81d3a36f715da325c7301061dac1f92b21196
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-02-26 13:55:48 +0200

    Disable CSRF forgery.

---
+++ FrontEnd/src/main/java/webserver/WebSecurityConfigDisable.java
@@ -0,0 +1,18 @@
+package webserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigDisable extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
