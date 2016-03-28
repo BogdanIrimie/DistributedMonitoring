9970c9a28faa05b22be6efcff1aad47aec3ba630
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-03-28 19:07:43 +0300

    Remove PID file after stoping or attempting to stop the process.

--- deployment/specs_monitoring_nmap_frontend/bin/stop.sh
+++ deployment/specs_monitoring_nmap_frontend/bin/stop.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+PID_FILE="../var/specs_monitoring_nmap_frontend.pid"
+
 # Check if PID file exists.
-if [ ! -f ../var/specs_monitoring_nmap_frontend.pid ];
+if [ ! -f $PID_FILE ];
 then
     echo "No PID file found!"
     exit -1
 fi
 
 # Read PID from file.
-file="../var/specs_monitoring_nmap_frontend.pid"
-PID=$(cat "$file")
+PID=$(cat "$PID_FILE")
 
 # Check if file content is a number.
 numberRegex='^[0-9]+$'
 if ! [[ $PID =~ $numberRegex ]];
 then
     echo "The content of the file is not a valid PID!"
+    echo "The file will be removed!"
+    rm $PID_FILE
     exit -2
 fi
 
@@ -24,5 +27,9 @@ if kill $PID;
 then
     echo "Process with PID $PID was killed!"
 else
-    echo "There is no process with $PID running!"
+    echo "There is no process with PID $PID running!"
 fi
+
+# Remove PID file
+echo "Removing PID file!"
+rm $PID_FILE
--- deployment/specs_monitoring_nmap_presenter/bin/stop.sh
+++ deployment/specs_monitoring_nmap_presenter/bin/stop.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+PID_FILE="../var/specs_monitoring_nmap_presenter.pid"
+
 # Check if PID file exists.
-if [ ! -f ../var/specs_monitoring_nmap_presenter.pid ];
+if [ ! -f $PID_FILE ];
 then
     echo "No PID file found!"
     exit -1
 fi
 
 # Read PID from file.
-file="../var/specs_monitoring_nmap_presenter.pid"
-PID=$(cat "$file")
+PID=$(cat "$PID_FILE")
 
 # Check if file content is a number.
 numberRegex='^[0-9]+$'
 if ! [[ $PID =~ $numberRegex ]];
 then
     echo "The content of the file is not a valid PID!"
+    echo "The file will be removed!"
+    rm $PID_FILE
     exit -2
 fi
 
@@ -26,3 +29,7 @@ then
 else
     echo "There is no process with $PID running!"
 fi
+
+# Remove PID file
+echo "Removing PID file!"
+rm $PID_FILE
