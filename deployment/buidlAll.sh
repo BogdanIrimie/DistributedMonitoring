e1e986deba88291fb357ce0d4b1a18669aa45152
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-03-28 20:06:04 +0300

    Add Scheduler component to build script.

--- deployment/buidlAll.sh
+++ deployment/buidlAll.sh
@@ -9,6 +9,10 @@ echo ------ Start building FrontEnd component ------
 mvn -Dmaven.test.skip=true package -f $projectPath/FrontEnd;
 echo ------ Finish FrontEnd component build ------
 
+echo ------ Start building Scheduler component ------
+mvn -Dmaven.test.skip=true package -f $projectPath/Scheduler;
+echo ------ Finish Scheduler component build ------
+
 echo ------ Start building Scanner component ------
 mvn -Dmaven.test.skip=true package -f $projectPath/Scanner;
 echo ------ Finish Scanner component build ------
@@ -23,18 +27,23 @@ echo ------ Finish Presenter component build ------
 
 # Copy .jar files to deployment folder.
 cp $projectPath/FrontEnd/target/FrontEnd.jar $projectPath/deployment/specs_monitoring_nmap_frontend/lib
+cp $projectPath/Scheduler/target/Scheduler.jar $projectPath/deployment/specs_monitoring_nmap_scheduler/lib
 cp $projectPath/Scanner/target/Scanner.jar $projectPath/deployment/specs_monitoring_nmap_scanner/lib
 cp $projectPath/Converter/target/Converter.jar $projectPath/deployment/specs_monitoring_nmap_converter/lib
 cp $projectPath/Presenter/target/Presenter.jar $projectPath/deployment/specs_monitoring_nmap_presenter/lib
 
 # Remove all logs from the deployment archives.
 rm -rf $projectPath/deployment/specs_monitoring_nmap_frontend/var/*
+rm -rf $projectPath/deployment/specs_monitoring_nmap_scheduler/var/*
 rm -rf $projectPath/deployment/specs_monitoring_nmap_scanner/var/*
 rm -rf $projectPath/deployment/specs_monitoring_nmap_converter/var/*
 rm -rf $projectPath/deployment/specs_monitoring_nmap_presenter/var/*
 
 # Create deployment archives for all components.
 tar -czf deployment/specs_monitoring_nmap_frontend.tar.gz -C $projectPath/deployment ./specs_monitoring_nmap_frontend
+tar -czf deployment/specs_monitoring_nmap_scheduler.tar.gz -C $projectPath/deployment ./specs_monitoring_nmap_scheduler
 tar -czf deployment/specs_monitoring_nmap_scanner.tar.gz -C $projectPath/deployment ./specs_monitoring_nmap_scanner
 tar -czf deployment/specs_monitoring_nmap_converter.tar.gz -C $projectPath/deployment ./specs_monitoring_nmap_converter
 tar -czf deployment/specs_monitoring_nmap_presenter.tar.gz -C $projectPath/deployment ./specs_monitoring_nmap_presenter
+
+echo ------ Build finished ------
