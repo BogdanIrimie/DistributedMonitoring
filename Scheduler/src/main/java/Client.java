f02bb65b078b5c46d548ded6876b37eddda7adb1
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-06-07 11:36:25 +0200

    Add argument parser to Scheduler component.

--- Scheduler/src/main/java/Client.java
+++ Scheduler/src/main/java/Client.java
@@ -1,9 +1,29 @@
+import dmon.core.commons.helpers.CommandLineArgumentParser;
+import dmon.core.commons.helpers.PidManipulation;
+import dmon.core.commons.helpers.ProgramArguments;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import rabbit.Receiver;
 
 public class Client {
+    public static final Logger logger = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
+        MDC.put("pid", PidManipulation.getPid());
+        logger.info("Scanner is started!");
+
+        new CommandLineArgumentParser(args).parse();
+        if (ProgramArguments.getPidFile() != null) {
+            PidManipulation.writeOwnPidToFile(ProgramArguments.getPidFile());
+        }
+        else {
+            PidManipulation.writeOwnPidToFile("../var/specs_monitoring_nmap_scheduler.pid");
+        }
+
         Receiver receiver = new Receiver();
         receiver.startReceiving();
+
+        MDC.clear();
     }
 }
